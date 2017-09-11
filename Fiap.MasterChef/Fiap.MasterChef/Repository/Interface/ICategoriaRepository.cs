91b22d11fe32c7fae3d45fa4945f06d9bc488ec4
Felipe Berlini <felipe.berlini@gmail.com>
2017-09-11 20:45:07 -0300

    Inclusão de interfaces de repositório

---
+++ Fiap.MasterChef/Fiap.MasterChef/Repository/Interface/ICategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fiap.MasterChef.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.MasterChef.Repository.Interface
+{
+    public interface ICategoriaRepository
+    {
+        void Salvar(CategoriaModel categoria);
+        void Deletar(Int32 id);
+        void Editar(CategoriaModel categoria);
+        List<CategoriaModel> BuscarTodos();
+        CategoriaModel Buscar();
+    }
+}
---
+++ Fiap.MasterChef/Fiap.MasterChef/Repository/Interface/IReceitaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Fiap.MasterChef.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiap.MasterChef.Repository.Interface
+{
+    public interface IReceitaRepository
+    {
+        void Salvar(ReceitaModel receita);
+        void Deletar(Int32 id);
+        void Editar(ReceitaModel receita);
+        List<ReceitaModel> BuscarTodos();
+        ReceitaModel Buscar();
+    }
+}
